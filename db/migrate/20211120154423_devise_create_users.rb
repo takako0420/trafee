2a804cfd3e000517b8c6ec797346017fc0b1959a
takako0420 <marl0420boro@gmail.com>
2021-11-21 05:18:35 +0900

    カラム追加

--- db/migrate/20211120154423_devise_create_users.rb
+++ db/migrate/20211120154423_devise_create_users.rb
@@ -4,6 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :family_name,        null: false
+      t.string :first_name,         null: false
+      t.string :family_name_kana,   null: false
+      t.string :first_name_kana,    null: false
+      t.string :company,            null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,11 @@
 ActiveRecord::Schema.define(version: 2021_11_20_154423) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "company", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
