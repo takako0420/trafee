11e7f39b120d705165873064fd248109bba6a499
takako0420 <marl0420boro@gmail.com>
2021-11-21 02:27:45 +0900

    ログイン画面などの変更

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,16 @@
+module DeviseHelper
+  def bootstrap_devise_error_messages!
+    return "" if resource.errors.empty?
+
+    html = ""
+    resource.errors.full_messages.each do |error_message|
+      html += <<-EOF
+      <div class="alert alert-danger alert-dismissible" role="alert">
+        </button>
+        #{error_message}
+      </div>
+      EOF
+    end
+    html.html_safe
+  end
+end
