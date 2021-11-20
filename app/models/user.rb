37e27ece00e4091d94e7e423284090e3ea68ebb2
takako0420 <marl0420boro@gmail.com>
2021-11-21 07:33:45 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
+  with_options presence: true do
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :company
+  end
+
+  validates :family_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, allow_blank: true
+  validates :first_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, allow_blank: true
+  validates :family_name_kana,format: { with: /\A[ァ-ヶー]+\z/ }, allow_blank: true
+  validates :first_name_kana,format: { with: /\A[ァ-ヶー]+\z/ }, allow_blank: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, allow_blank: true
+
 end
