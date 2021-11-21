72e3d8529af7a7e3ec2a39221d30ddc2ff2843ce
takako0420 <marl0420boro@gmail.com>
2021-11-22 00:19:37 +0900

    user単体テストの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     family_name_kana      { 'ミョウジ' }
     first_name_kana       { 'ナマエ' }
     company               { '株式会社123' }
-    email                 {Faker::Internet.free_email}
-    password              { '1a' + Faker::Internet.password(min_length: 4,max_length: 30) }
-    password_confirmation {password}
+    password              { '111aaa' }
+    password_confirmation { password }
+    email                 { Faker::Internet.free_email }
   end
 end
