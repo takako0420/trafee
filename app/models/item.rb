39d257aff9a9e70eab8bed87ef74e3c77d8f4ff3
takako0420 <marl0420boro@gmail.com>
2021-12-01 21:21:04 +0900

    formオブジェクトからcocoonに変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
+  validates :text, presence: true, length: { maximum: 100 }
   belongs_to :sheet
 end
--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,4 +1,11 @@
 class Sheet < ApplicationRecord
+
+  with_options presence: true do
+    validates :user_id
+    validates :title, length: { maximum: 40 }
+  end
+
   belongs_to :user
-  has_many :items, dependent: :destroy 
+  has_many :items, dependent: :destroy
+  accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
 end
