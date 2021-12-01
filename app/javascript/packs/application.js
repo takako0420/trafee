a9ce1cb45cf3c266ed420e7b29c84ac8b3e1f24f
takako0420 <marl0420boro@gmail.com>
2021-12-01 13:48:56 +0900

    cocoon導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("@nathanvda/cocoon")
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
