2d0f8b25201dc501572cf113046013e3ad84905d
takako0420 <marl0420boro@gmail.com>
2021-12-01 12:20:25 +0900

    devise編集機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :company])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :company])
   end
 
   def basic_auth
