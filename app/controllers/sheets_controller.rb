612566fb20d439cbe0cf261c83da9fce12d53e9e
takako0420 <marl0420boro@gmail.com>
2021-12-03 13:32:28 +0900

    コントローラー設定

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,27 +1,75 @@
 class SheetsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  # before_action :move_to_index, only: [:new, :create]
+  before_action :authenticate_user!
+  before_action :set_item, only:[:edit, :update, :show, :destroy]
+  before_action :move_to_index, only:[:edit, :show, :destroy ]
 
   def index
+    @sheets = current_user.sheets.order(created_at: :desc)
   end
 
   def new
-    @sheet_item = SheetItem.new
+    @sheet = Sheet.new
+    @sheet.items.build
   end
 
   def create
-    @sheet_item = SheetItem.new(sheet_params)
-    if @sheet_item.valid?
-      @sheet_item.save
-      redirect_to root_path
+    if Sheet.create(sheet_params)
+      redirect_to action: :index
+      flash[:info] = "スキルマップを登録しました。"
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @sheet.update(update_sheet_params)
+      redirect_to action: :index
+      flash[:info] = "スキルマップを更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @sheet.destroy
+      redirect_to sheets_path(current_user.id)
+      flash[:info] = "スキルマップを削除しました。"
+    else
+      render :index
+    end
+  end
+
+  def show
+  end
+
   private
 
+  def set_item
+    @sheet = current_user.sheets.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index if @sheet.user_id != current_user.id
+  end
+
   def sheet_params
-    params.require(:sheet_item).permit(:title, :text).merge(user_id: current_user.id)
+    params.require(:sheet).permit(:title,
+      items_attributes:[
+        :sheet_id,
+        :text,
+        ]).merge(user_id: current_user.id)
+  end
+
+  def update_sheet_params
+    params.require(:sheet).permit(:title,
+      items_attributes:[
+        :id,
+        :sheet_id,
+        :text,
+        :_destroy
+        ]).merge(user_id: current_user.id)
   end
 end
