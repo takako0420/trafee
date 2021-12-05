cb11c1e282c003c443081a061eb01aefe3f2f0b3
takako0420 <marl0420boro@gmail.com>
2021-12-05 10:00:08 +0900

    rubocop

--- spec/factories/sheets.rb
+++ spec/factories/sheets.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :sheet do
-    title { "スキルマップのタイトル" }
+    title { 'スキルマップのタイトル' }
     association :user
   end
 
   factory :sheet_with_item, class: Sheet do
-    title { "スキルマップのタイトル" }
-    after( :create ) do |sheet|
+    title { 'スキルマップのタイトル' }
+    after(:create) do |sheet|
       create :item, sheet: sheet
     end
   end
--- spec/models/sheet_spec.rb
+++ spec/models/sheet_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Sheet, type: :model do
   before do
-    user = FactoryBot.create( :user )
-    @item = FactoryBot.build( :item )
-    @sheet = FactoryBot.build( :sheet, user_id: user.id )
-    @sheet_item = FactoryBot.create( :sheet_with_item, user_id: user.id )
+    user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item)
+    @sheet = FactoryBot.build(:sheet, user_id: user.id)
+    @sheet_item = FactoryBot.create(:sheet_with_item, user_id: user.id)
   end
 
   describe 'スキルマップ保存' do
@@ -20,7 +20,7 @@ RSpec.describe Sheet, type: :model do
 
     context 'スキルマップを登録できないとき' do
       it 'タイトルがないと登録できない' do
-        @sheet_item.title = ""
+        @sheet_item.title = ''
         @sheet_item.valid?
         expect(@sheet_item.errors.full_messages).to include('Titleを入力してください')
       end
@@ -30,12 +30,12 @@ RSpec.describe Sheet, type: :model do
         expect(@sheet_item.errors.full_messages).to include('Userを入力してください')
       end
       it 'タイトルは40文字以内でないと登録できない' do
-        @sheet_item.title = "a" * 41
+        @sheet_item.title = 'a' * 41
         @sheet_item.valid?
         expect(@sheet_item.errors.full_messages).to include('Titleは40文字以内で入力してください')
       end
       it 'テキストは100文字以内でないと登録できない' do
-        @item.text = "a" * 101
+        @item.text = 'a' * 101
         @item.valid?
         expect(@item.errors.full_messages).to include('Textは100文字以内で入力してください')
       end
