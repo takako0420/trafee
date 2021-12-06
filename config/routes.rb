f7376b1189e28bc77c8412666a669e86897e6292
takako0420 <marl0420boro@gmail.com>
2021-12-06 18:46:48 +0900

    documentsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'documents/new'
   root 'users#index'
   devise_for :users
   resources :sheets 
---
+++ spec/requests/documents_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Documents", type: :request do
+  describe "GET /new" do
+    it "returns http success" do
+      get "/documents/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
