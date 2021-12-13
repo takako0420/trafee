0d146e179b310ac10bb9e159488bcc43fff718b5
takako0420 <marl0420boro@gmail.com>
2021-12-13 12:01:49 +0900

    documentバリデーション修正

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,13 +2,12 @@ class Document < ApplicationRecord
   with_options presence: true do
     validates :user_id
     validates :sheet_id
-  end
-
-  with_options numericality: { only_integer: true }  do
-    validates :chose_one
-    validates :chose_two
-    validates :chose_three
-    validates :chose_four
+    with_options numericality: { only_integer: true }  do
+      validates :chose_one
+      validates :chose_two
+      validates :chose_three
+      validates :chose_four
+    end
   end
 
   belongs_to :user
