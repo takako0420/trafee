d2e1aa122f3b736673f07e8bdad7622683fc103f
takako0420 <marl0420boro@gmail.com>
2021-12-16 18:12:38 +0900

    rubocop

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ApplicationRecord
   with_options presence: true do
     validates :user_id
     validates :sheet_id
-    with_options numericality: { only_integer: true }  do
+    with_options numericality: { only_integer: true } do
       validates :chose_one
       validates :chose_two
       validates :chose_three
--- spec/requests/documents_spec.rb
+++ spec/requests/documents_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Documents", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/documents/new"
+RSpec.describe 'Documents', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/documents/new'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
