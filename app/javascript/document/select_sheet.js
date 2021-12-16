99cd1dbd048e062d271ff1f1633b7f2e28ecb08f
takako0420 <marl0420boro@gmail.com>
2021-12-16 10:00:43 +0900

    JSシート連動

--- app/javascript/document/select_sheet.js
+++ app/javascript/document/select_sheet.js
@@ -1,13 +1,46 @@
-const s_Input = document.getElementById('select-main');
-const s_Send = document.getElementById('form-select');
-const changeText = document.getElementById('change');
+const selectInput = document.getElementById('select-main');
+const selectHidden = document.getElementById('form-select');
+const tableText = document.getElementsByClassName('sheet-text');
+const inputTable = document.getElementsByClassName('input-table');
+const resetTable = document.getElementById('reset');
 
-//セレクトボックス連動
 function valueChange(){
-  let c_Num = s_Input.selectedIndex;
-  if (c_Num != 0 ){
-  s_Send.options[c_Num].selected = true;
+  let c_Num = selectInput.selectedIndex;
+  if (c_Num != 0){
+  selectHidden.options[c_Num].selected = true;
+  searchSheetId(c_Num)
   }
 };
 
-if(s_Input != null){s_Input.addEventListener('change', valueChange)};
+function searchSheetId(c_Num) {
+  let i = 0, len = 0, itemCount = 0;
+  for (i = 0, len = gon.items.length; i < len; i++) {
+    if (gon.sheets[c_Num-1].id === gon.items[i].sheet_id) {
+        addTableChange(itemCount, gon.items[i].text)
+        itemCount++;
+    }
+  }
+};
+
+function addTableChange(count, text){
+  if (count === 0){
+    tableText[0].innerText = text;
+  } else {
+    cloneArea = inputTable[0].cloneNode(true);
+    inputTable[inputTable.length - 1].after(cloneArea);
+    tableText[tableText.length - 1].innerText = text;
+  }
+};
+
+let defaultHTML;
+function DefaultSave() {
+	defaultHTML = resetTable.innerHTML;
+}
+
+function HTMLRestore() {
+	resetTable.innerHTML = defaultHTML;
+  valueChange()
+}
+
+window.addEventListener('load', DefaultSave);
+if(selectInput != null){selectInput.addEventListener('change', HTMLRestore)};
