1c4509331a2f1251b1d4772d1c0b836aa48f9983
takako0420 <marl0420boro@gmail.com>
2021-12-16 17:56:43 +0900

    documentビューを作成

--- app/javascript/document/point.js
+++ app/javascript/document/point.js
@@ -1,47 +1,52 @@
-const group = document.getElementsByClassName("btn-group");
-const btn = document.getElementsByName('check');
+const selectBox = document.getElementById('select-main');
+let group = document.getElementsByClassName("btn-group");
+let btn = document.getElementsByName('check');
 const form = document.getElementsByClassName('chose');
+const tableWrap = document.getElementById('reset');
 
-//ボタンの順番を取得
-for (let i = btn.length - 1 ; i >= 0 ; i--){
-  let btnNum = (i + 4) % 4;
-  btnSet(btnNum)
-  btn[i].addEventListener("click", function(){
-  //アクティブ判定
-  if(btn[i].classList.contains('active') === true){ 
-    changeActive(i,btnNum);
-  }else{
-    score(0,btnNum);
-    divideCount(0,btnNum);
-  }
-})};
+//テーブル監視
+var observer = new MutationObserver(function(){
+  formSet();
+  outPut(0);
+  divideCount(0,'reset');
+  score('reset',0)
+  btnSearch();
+})
 
-//フォーム初期値０
-function btnSet(btnNum){
-  let n = 0;
-  while (n <= btnNum){
-    form[n].value = 0;
-    n++;
-  }
+const config = { 
+  childList: true
 };
 
-//グループ内アクティブを非アクティブに切り替え
-function changeActive (i,btnNum){
-  //行番号
-  let rowNum = Math.ceil(( i + 5 ) / 4 - 2);
-  //クリックしたボタンの順序
+observer.observe(tableWrap, config);
 
+//クリックイベント
+function btnSearch(){
+  for (let i = btn.length - 1 ; 0 <= i ; i--){
+    let btnNum = (i + 4) % 4;
+
+    btn[i].addEventListener("click", () => {
+      if(btn[i].classList.contains('active')){ 
+        changeActive(i,btnNum);
+      }else{
+        score(0,btnNum);
+        divideCount(0,btnNum);
+      }
+    })
+  }
+}
+
+//アクティブ切り替え
+function changeActive (i,btnNum){
   score(1,btnNum);
   divideCount(1,btnNum);
-  //クリックしたボタンは削除対象から除外
+  let rowNum = Math.ceil(( i + 5 ) / 4 - 2);
   let searchActive = [0, 1, 2, 3];
   searchActive.splice(btnNum, 1);
-  //アクティブを非アクティブに切り替え
   let c = 0;
   while (c < 3){
     searchActive.forEach( e => {
       let target = group[rowNum].children[e].classList;
-      if(target.contains('active') === true){ 
+      if(target.contains('active')){ 
         target.remove('active');
         score(0,e);
         divideCount(0,e);
@@ -49,34 +54,40 @@ function changeActive (i,btnNum){
     })
     c++
   }
-};
+}
 
-//合計点振り分け
-const checkPoint = 100 / btn.length;
+//合計点
 let numValue = 0;
-
 function score(x,count){
   if (x === 1){
-    numValue = numValue + ((count + 1) * checkPoint);
+    numValue = numValue + ((count + 1) * (100 / btn.length));
     outPut(numValue);
   } else if (x === 0){
-    numValue = numValue - ((count + 1) * checkPoint);
+    numValue = numValue - ((count + 1) * (100 / btn.length));
     outPut(numValue);
+  } else if (x === 'reset'){
+    numValue = count;
   }
-};
+}
 
-//合計点出力
 function outPut(numValue){
   document.getElementById('sum').textContent = Math.round(numValue);
-};
+}
 
-//フォームのカウント初期値
+//フォーム
+function formSet(){
+  let n = 0;
+  while (n < 4){
+    form[n].value = 0;
+    n++;
+  }
+}
+
 let c_one = 0;
 let c_two = 0;
 let c_three = 0;
 let c_four = 0;
 
-//フォーム振り分け
 function divideCount(x,btnNum){
   switch (btnNum){
     case 0:
@@ -95,10 +106,12 @@ function divideCount(x,btnNum){
       (x===1)?c_four++:c_four--;
       minNum(c_four,btnNum);
       break;
+    case 'reset':
+      c_one = x, c_two = x, c_three = x, c_four = x;
+      break;
   }
-};
+}
 
-//フォームに出力
-function minNum (sumCount,btnNum){
-  form[btnNum].value = sumCount;
-};
+function minNum (count,btnNum){
+  form[btnNum].value = count;
+}
--- app/javascript/document/select_sheet.js
+++ app/javascript/document/select_sheet.js
@@ -3,24 +3,28 @@ const selectHidden = document.getElementById('form-select');
 const tableText = document.getElementsByClassName('sheet-text');
 const inputTable = document.getElementsByClassName('input-table');
 const resetTable = document.getElementById('reset');
+const selectZero = document.getElementById('select-except-zero');
 
 function valueChange(){
   let c_Num = selectInput.selectedIndex;
   if (c_Num != 0){
-  selectHidden.options[c_Num].selected = true;
-  searchSheetId(c_Num)
+    selectZero.style.display = "block";
+    selectHidden.options[c_Num].selected = true;
+    searchSheetId(c_Num);
+  } else if (c_Num === 0){
+    selectZero.style.display = "none";
   }
-};
+}
 
 function searchSheetId(c_Num) {
   let i = 0, len = 0, itemCount = 0;
   for (i = 0, len = gon.items.length; i < len; i++) {
     if (gon.sheets[c_Num-1].id === gon.items[i].sheet_id) {
-        addTableChange(itemCount, gon.items[i].text)
+        addTableChange(itemCount, gon.items[i].text);
         itemCount++;
     }
   }
-};
+}
 
 function addTableChange(count, text){
   if (count === 0){
@@ -30,16 +34,17 @@ function addTableChange(count, text){
     inputTable[inputTable.length - 1].after(cloneArea);
     tableText[tableText.length - 1].innerText = text;
   }
-};
+}
 
 let defaultHTML;
 function DefaultSave() {
+  selectZero.style.display = "none";
 	defaultHTML = resetTable.innerHTML;
 }
 
 function HTMLRestore() {
 	resetTable.innerHTML = defaultHTML;
-  valueChange()
+  valueChange();
 }
 
 window.addEventListener('load', DefaultSave);
