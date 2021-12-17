4e3cf9fb3705f7874890bdbbd1f7c28aa4fbedc0
takako0420 <marl0420boro@gmail.com>
2021-12-17 22:13:29 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
   resources :sheets
-  resources :documents, only: [:new, :create] do
-    resources :comments, only: [:new, :create]
-  end
+  resources :documents, only: [:new, :create]
+  resources :comments, only: [:new, :create]
 end
