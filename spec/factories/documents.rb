d6b57d886d9e3bdca424da0f65b7d2a8138e9e7d
takako0420 <marl0420boro@gmail.com>
2021-12-17 15:39:51 +0900

    documentモデルテスト

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :document do
+    chose_one   {rand(10)}
+    chose_two   {rand(10)}
+    chose_three {rand(10)}
+    chose_four  {rand(10)}
+    association :user, :sheet
   end
 end
